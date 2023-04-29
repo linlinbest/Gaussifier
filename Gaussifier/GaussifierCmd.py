Finish covariance visualization and fix some bugs; update UI

Gaussifier/GaussifierCmd.py
@@ -4,6 +4,7 @@ import maya.cmds as cmds
 import igl
 import numpy as np
 import re
+import copy
 
 
 def getGPSData():
@@ -18,6 +19,10 @@ def getData():
     global faceData
     return vertexData, faceData
 
+def getInvCovs():
+    global invCovs
+    return invCovs
+
 def setCovarianceAt(i, covStr):
     global invCovs
     covStr = covStr.replace('\n', '').replace('[', '').replace(']', '')
@@ -30,6 +35,11 @@ def setCovarianceAt(i, covStr):
 def getCovarianceAt(i):
     global invCovs
     return np.linalg.inv(invCovs[i])
+
+def getInitCovarianceAt(i):
+    global initInvCovs
+    return np.linalg.inv(initInvCovs[i])
+
 
 def loadMesh(mesh):
     global vertexData
@@ -56,13 +66,29 @@ def loadMesh(mesh):
 
     print("Selected mesh is successfully loaded.")
 
-    generateInvCov()
+    generateIndentityCov()
+    # generateInvCov()
+
+
+
+def generateIndentityCov():
+    global vertexData
+    global invCovs
+    global initInvCovs
+
+    # creating covariance matrices with diagonal values as 0.01
+    identityMat = np.identity(3) * 100.0
+    invCovs = np.tile(identityMat, (len(vertexData), 1, 1))
+
+    # invCovs = np.zeros((len(vertexData), 3, 3))
+    initInvCovs = copy.deepcopy(invCovs)
 
 
 def generateInvCov():
     global vertexData
     global faceData
     global invCovs
+    global initInvCovs
 
     invCovs = np.zeros((len(vertexData), 3, 3))
 
@@ -84,6 +110,7 @@ def generateInvCov():
         for fv in f:
             invCovs[fv] += np.linalg.inv(cov)
 
+    initInvCovs = copy.deepcopy(invCovs)
     print("Covariances are successfully generated.")
 
 

Gaussifier/GaussifierDialog.py
@@ -1,10 +1,21 @@
 import maya.cmds as cmds
 import GaussifierCmd
+import GaussifierNode
 import numpy as np
 
 from functools import partial
 
 
+global currNumSubdiv
+currNumSubdiv = int(1)
+
+def getCurrNumSubdiv():
+    global currNumSubdiv
+    return currNumSubdiv
+
+def setCurrNumSubdiv(numSubdiv):
+    global currNumSubdiv
+    currNumSubdiv = numSubdiv
 
 
 def createMenu():
@@ -41,6 +52,9 @@ def loadMesh(melArg):
     else:
         print("No mesh is selected.")
 
+def generateCovariances(melArg):
+    GaussifierCmd.generateInvCov()
+
 
 def generateMesh(melArg):
     createNode()
@@ -50,11 +64,11 @@ def generateMesh(melArg):
 def setCovariance(scrollField):
     global vertIndex
     covStr = cmds.scrollField(scrollField, q=True, text=True)
-    print("covStr inSetCovariance: " + covStr)
+    # print("covStr inSetCovariance: " + covStr)
     GaussifierCmd.setCovarianceAt(vertIndex, covStr)
-    print("Before Generate Mesh")
-    GaussifierCmd.generateMesh(1)
-    cmds.setAttr("GaussifierNode1.numSubdivisions", 1)
+    # print("Before Generate Mesh")
+    # GaussifierCmd.generateMesh(GaussifierNode.getCurrNumSubdiv())
+    cmds.setAttr("GaussifierNode1.numSubdivisions", getCurrNumSubdiv())
 
 
 
@@ -62,25 +76,28 @@ def createDialogWindow(melArg):
     dialogWindow = cmds.window(title="Gaussifier", widthHeight=(500, 500), rtf=True)
 
     cmds.frameLayout(label=" ")
-    cmds.rowLayout(nc=2, adjustableColumn=1)
-    cmds.text(label="Browse for input mesh files")
-    cmds.button(label="Browse", w=120, h=20, command=loadMesh)
-    cmds.setParent("..")
+    # cmds.rowLayout(nc=2, adjustableColumn=1)
+    # cmds.text(label="Browse for input mesh files")
+    # cmds.button(label="Browse", w=120, h=20, command=loadMesh)
+    # cmds.setParent("..")
     cmds.button(label="Load selected Mesh", w=120, h=20, command=loadMesh)
+    cmds.button(label="Generate covariances", w=120, h=20, command=generateCovariances)
     cmds.setParent("..")
     
     cmds.frameLayout(label="Selected vertex")
     selectedVertexScrollField = cmds.scrollField(wordWrap=True, editable=True)
     cmds.button(label="Update Mesh", w=120, h=20, command=lambda melArg: setCovariance(selectedVertexScrollField))
+    cmds.button(label="Enable covariance visualization", w=120, h=20, command=enableCovVisualization)
+    cmds.button(label="Disable covariance visualization", w=120, h=20, command=disableCovVisualization)
     cmds.setParent("..")
     
-    cmds.frameLayout(label="Selected covariance parameters", collapsable=True, collapse=False)
-    cmds.columnLayout() 
-    cmds.floatSliderGrp("xSlider", label="X", field=True, min=0.0, max=1.0)
-    cmds.floatSliderGrp("ySlider", label="Y", field=True, min=0.0, max=1.0)
-    cmds.floatSliderGrp("zSlider", label="Z", field=True, min=0.0, max=1.0)
-    cmds.setParent("..")
-    cmds.setParent("..")
+    # cmds.frameLayout(label="Selected covariance parameters", collapsable=True, collapse=False)
+    # cmds.columnLayout() 
+    # cmds.floatSliderGrp("xSlider", label="X", field=True, min=0.0, max=1.0)
+    # cmds.floatSliderGrp("ySlider", label="Y", field=True, min=0.0, max=1.0)
+    # cmds.floatSliderGrp("zSlider", label="Z", field=True, min=0.0, max=1.0)
+    # cmds.setParent("..")
+    # cmds.setParent("..")
 
     buttonsForm = cmds.formLayout(numberOfDivisions=100)
     generateButton = cmds.button(label="Generate", w=100, h=40, command=generateMesh)
@@ -94,25 +111,33 @@ def createDialogWindow(melArg):
 
 
 def onScaleChanged(scrollField):
-    print("Selected Cov Scale Changed!") 
-    global covStr
-    global selectedCov
-    covStr = cmds.scrollField(scrollField, q=True, text=True)
-    covStr = covStr.replace('\n', '').replace('[', '').replace(']', '')
-    cov = np.fromstring(covStr, sep=' ', dtype= float)
-    cov = cov.reshape((3, 3))
-    selectedScale = cmds.getAttr(selectedCov + '.scale')
-    print("selected Scale: " + str(selectedScale[0][0]) + ", " + str(selectedScale[0][1]), ", " + str(selectedScale[0][2]))
+    # print("Selected Cov Scale Changed!") 
 
+    global selectedCov
+    global vertIndex
+
+    cov = np.empty((3,3), dtype=float)
+    initCov = GaussifierCmd.getInitCovarianceAt(vertIndex)
+
+    selectedScale = cmds.getAttr(selectedCov + '.scale')
+    # print("selected Scale: " + str(selectedScale[0][0]) + ", " + str(selectedScale[0][1]), ", " + str(selectedScale[0][2]))
+
+    scale = [1.0, 1.0, 1.0]
+    # Let scaling have greater effect
+    for i in range(len(selectedScale[0])):
+        if abs(selectedScale[0][i]) > 1.0:
+            scale[i] = selectedScale[0][i] * 10.0
+        else:
+            scale[i] = selectedScale[0][i] / 10.0
 
     #3D to 3x3 conversion
-    cov[0][0] =  cov[0][0] * selectedScale[0][0]
-    cov[1][1] =  cov[1][1] * selectedScale[0][1]
-    cov[2][2] =  cov[2][2] * selectedScale[0][2]
+    cov[0][0] =  initCov[0][0] * scale[0]
+    cov[1][1] =  initCov[1][1] * scale[1]
+    cov[2][2] =  initCov[2][2] * scale[2]
 
-    np.set_printoptions(formatter={'float': lambda x: "{:.3f}".format(x)})
+    np.set_printoptions(formatter={'float': lambda x: "{:.5f}".format(x)})
     covStr = np.array2string(cov)
-    print("covStr onScaleChanged: " + covStr)
+    # print("covStr onScaleChanged: " + covStr)
     cmds.scrollField(scrollField, edit=True, text=covStr)
     
 
@@ -121,24 +146,33 @@ def onScaleChanged(scrollField):
 def updateScrollfield(scrollField, *args):
     global nameToIndex
     global selectedCov
-    print("Selection Changed")
+    # print("Selection Changed")
     selected_objects = cmds.ls(selection=True)
     if selected_objects:
         selectedCov = selected_objects[0]
-        if not selectedCov.startswith("pCube"):
-            return
-        cmds.scriptJob(attributeChange=[selectedCov + '.scale', lambda: onScaleChanged(scrollField)])
-        #start = indexOf("[", selectedVertex)
 
-        #if start == -1:
-            #return 
         global vertIndex
         global covStr
-        #end = indexOf("]", selectedVertex)
-        #vertIndexStr = selectedVertex[start + 1:end]
-        vertIndex = nameToIndex[selectedCov]
+
+        # select a covariance vertex
+        if ".vtx[" in selectedCov:
+            start = indexOf("[", selectedCov)
+
+            if start == -1:
+                return 
+            
+            end = indexOf("]", selectedCov)
+            vertIndexStr = selectedCov[start + 1:end]
+            vertIndex = int(vertIndexStr)
+
+        # select a covariance cube
+        elif selectedCov.startswith("pCube"):
+            cmds.scriptJob(attributeChange=[selectedCov + '.scale', lambda: onScaleChanged(scrollField)])
+            vertIndex = nameToIndex[selectedCov]
+        
         covStr = np.array2string(GaussifierCmd.getCovarianceAt(vertIndex), precision=4)
         cmds.scrollField(scrollField, edit=True, text=covStr)
+
     else:
         cmds.scrollField(scrollField, edit=True, text="")
 
@@ -158,16 +192,32 @@ def visualizeCovariances():
     nameToIndex = {}
     vertexData, _ = GaussifierCmd.getData()
 
+
+    # invCovs = GaussifierCmd.getInvCovs()
+
     for i in range(len(vertexData)):
         v = vertexData[i]
 
-        cube = cmds.polyCube(width=0.1, height=0.1, depth=0.1)[0]
-        print(cube)
+        cov = GaussifierCmd.getCovarianceAt(i)
+        cube = cmds.polyCube(width=cov[0][0]*10.0, height=cov[1][1]*10.0, depth=cov[2][2]*10.0)[0]
+        # print(cube)
         cmds.move(v[0], v[1], v[2], cube)
         cmds.parent(cube, "ControlMesh")
     
         cubes.append(cube)
         nameToIndex[cube] = i
-    
+
+    # disableCovVisualization(None)
 
     
+def enableCovVisualization(melArg):
+    global cubes
+    for cube in cubes:
+        cmds.setAttr(cube + ".visibility", True)
+        cmds.displaySurface(cube, xRay=True)
+    
+
+def disableCovVisualization(melArg):
+    global cubes
+    for cube in cubes:
+        cmds.setAttr(cube + ".visibility", False)